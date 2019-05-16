fe26520d60c7c795a8fbb941e3da946e6002b2e8
Erfan Alimohammadi <erfan.aa@gmail.com>
2019-05-16 20:49:23 +0430

    Change project name

--- text_to_commit_history.py
+++ text_to_commit_history.py
@@ -5,7 +5,7 @@ import itertools
 import json
 import math
 import os
-from text2matrix import get_arial_font_matrix
+from text_to_matrix import get_arial_font_matrix
 try:
     # Python 3+
     from urllib.error import HTTPError, URLError
@@ -27,11 +27,12 @@ FALLBACK_IMAGE = 'kitty'
 
 
 TITLE = '''
- _____         _   ____   ____                          _ _   
-|_   _|____  _| |_|___ \\ / ___|___  _ __ ___  _ __ ___ (_) |_ 
-  | |/ _ \\ \\/ / __| __) | |   / _ \\| '_ ` _ \\| '_ ` _ \\| | __|
-  | |  __/>  <| |_ / __/| |__| (_) | | | | | | | | | | | | |_ 
-  |_|\\___/_/\\_\\\\__|_____|\\____\\___/|_| |_| |_|_| |_| |_|_|\\__|
+ _____         _     _____        ____                          _ _     _   _ _     _                   
+|_   _|____  _| |_  |_   _|__    / ___|___  _ __ ___  _ __ ___ (_) |_  | | | (_)___| |_ ___  _ __ _   _ 
+  | |/ _ \\ \\/ / __|   | |/ _ \\  | |   / _ \\| '_ ` _ \\| '_ ` _ \\| | __| | |_| | / __| __/ _ \\| '__| | | |
+  | |  __/>  <| |_    | | (_) | | |__| (_) | | | | | | | | | | | | |_  |  _  | \\__ \\ || (_) | |  | |_| |
+  |_|\\___/_/\\_\\\\__|   |_|\\___/   \\____\\___/|_| |_| |_|_| |_| |_|_|\\__| |_| |_|_|___/\\__\\___/|_|   \\__, |
+                                                                                                  |___/
 
 '''
 
@@ -157,7 +158,7 @@ def generate_values_in_date_order(image, multiplier=1):
 def commit(commitdate):
     template = (
         '''GIT_AUTHOR_DATE={0} GIT_COMMITTER_DATE={1} '''
-        '''git commit --allow-empty -m "text2commit" > /dev/null\n'''
+        '''git commit --allow-empty -m "text_to_commit_history" > /dev/null\n'''
     )
     return template.format(commitdate.isoformat(), commitdate.isoformat())
 
@@ -170,8 +171,8 @@ def fake_it(image, start_date, username, repo, git_url, offset=0, multiplier=1):
         'cd $REPO\n'
         'touch README.md\n'
         'git add README.md\n'
-        'touch text2commit\n'
-        'git add text2commit\n'
+        'touch text_to_commit_history\n'
+        'git add text_to_commit_history\n'
         '{1}\n'
         'git remote add origin {2}:{3}/$REPO.git\n'
         'git pull origin master\n'
@@ -216,7 +217,7 @@ def main():
     m = calculate_multiplier(max_daily_commits)
 
     repo = request_user_input(
-        'Enter the name of the repository to use by text2commit: ')
+        'Enter the name of the repository to use by text_to_commit_history: ')
 
     offset = request_user_input(
         'Enter the number of weeks to offset the image (from the left): ')
@@ -224,20 +225,19 @@ def main():
     offset = int(offset) if offset.strip() else 0
 
     print((
-        'By default text2commit.py matches the darkest pixel to the highest\n'
+        'By default text_to_commit_history.py matches the darkest pixel to the highest\n'
         'number of commits found in your GitHub commit/activity calendar,\n'
         '\n'
         'Currently this is: {0} commits\n'
         '\n'
-        'Enter the word "text2commit" to exceed your max\n'
+        'Enter the word "text_to_commit_history" to exceed your max\n'
         '(this option generates WAY more commits)\n'
         'Any other input will cause the default matching behavior'
     ).format(max_daily_commits))
     match = request_user_input()
 
-    match = m if (match == 'text2commit') else 1
+    match = m if (match == 'text_to_commit_history') else 1
 
-    print('Enter file(s) to load images from (blank if not applicable)')
     img_names = "".split(' ')
 
     print('Enter your the text you want to be shown on your profile')
@@ -264,8 +264,8 @@ def main():
     output = fake_it(text_image, start_date, username, repo, git_url, offset,
                      fake_it_multiplier)
 
-    save(output, 'text2commit.sh')
-    print('text2commit.sh saved.')
+    save(output, 'text_to_commit_history.sh')
+    print('text_to_commit_history.sh saved.')
     print('Create a new(!) repo named {0} at {1} and run the script'.format(repo, git_base))
 
 
